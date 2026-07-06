d5f6d90cc38d0e7a8e2fec95fff001d10226c09f
Tenorio7z <victortenorio981@gmail.com>
2026-07-06 20:32:36 -0300

    correção de insert

--- services/notificacoes.py
+++ services/notificacoes.py
@@ -2,21 +2,28 @@ from database import conectar
 from datetime import datetime
 
 
-def notificar_gerente(funcionario, produto, valor, empresa_id):
+def notificar_gerente(usuario_id, produto, valor, empresa_id):
 
     conn = conectar()
     cursor = conn.cursor()
 
-    print("SALVANDO NOTIFICAÇÃO")
-    print("FUNCIONARIO:", funcionario)
-    print("PRODUTO:", produto)
-    print("VALOR:", valor)
-    print("EMPRESA:", empresa_id)
+    # Buscar nome do funcionário
+    cursor.execute("""
+        SELECT usuario
+        FROM usuarios
+        WHERE id = %s
+        AND empresa_id = %s
+    """, (usuario_id, empresa_id))
 
-    cursor.execute(
-        """
+    usuario = cursor.fetchone()
+
+    if usuario:
+        funcionario = usuario[0]
+    else:
+        funcionario = "Funcionário"
+
+    cursor.execute("""
         INSERT INTO notificacoes (
-
             empresa_id,
             funcionario,
             produto,
@@ -25,27 +32,20 @@ def notificar_gerente(funcionario, produto, valor, empresa_id):
             mensagem,
             data,
             lida
-
         )
-
         VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
-        """,
-        (
+    """, (
 
-            empresa_id,
-            funcionario,
-            produto,
-            valor,
-            "Nova Venda",
-            f"{funcionario} vendeu {produto} por R$ {valor:.2f}",
-            str(datetime.now()),
-            0
+        empresa_id,
+        funcionario,
+        produto,
+        valor,
+        "Nova Venda",
+        f"{funcionario} vendeu {produto} por R$ {valor:.2f}",
+        datetime.now(),
+        False
 
-        )
-    )
+    ))
 
     conn.commit()
-
-    print("NOTIFICAÇÃO GRAVADA")
-
     conn.close()
