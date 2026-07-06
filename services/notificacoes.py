1e91e173497979d9bf8eb5834c1813d14c338fde
Tenorio7z <victortenorio981@gmail.com>
2026-07-06 20:36:01 -0300

    correção de insert

--- services/notificacoes.py
+++ services/notificacoes.py
@@ -7,45 +7,45 @@ def notificar_gerente(usuario_id, produto, valor, empresa_id):
     conn = conectar()
     cursor = conn.cursor()
 
-    # Buscar nome do funcionário
-    cursor.execute("""
-        SELECT usuario
-        FROM usuarios
-        WHERE id = %s
-        AND empresa_id = %s
-    """, (usuario_id, empresa_id))
+    print("SALVANDO NOTIFICAÇÃO")
+    print("FUNCIONARIO:", usuario_id)
+    print("PRODUTO:", produto)
+    print("VALOR:", valor)
+    print("EMPRESA:", empresa_id)
 
-    usuario = cursor.fetchone()
-
-    if usuario:
-        funcionario = usuario[0]
-    else:
-        funcionario = "Funcionário"
-
-    cursor.execute("""
+    cursor.execute(
+        """
         INSERT INTO notificacoes (
+
             empresa_id,
-            funcionario,
+            usuario_id,
             produto,
             valor,
             titulo,
             mensagem,
             data,
             lida
+
         )
+
         VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
-    """, (
+        """,
+        (
 
-        empresa_id,
-        funcionario,
-        produto,
-        valor,
-        "Nova Venda",
-        f"{funcionario} vendeu {produto} por R$ {valor:.2f}",
-        datetime.now(),
-        False
+            empresa_id,
+            usuario_id,
+            produto,
+            valor,
+            "Nova Venda",
+            f"{usuario_id} vendeu {produto} por R$ {valor:.2f}",
+            str(datetime.now()),
+            0
 
-    ))
+        )
+    )
 
     conn.commit()
+
+    print("NOTIFICAÇÃO GRAVADA")
+
     conn.close()
